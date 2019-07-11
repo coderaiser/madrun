cc4ef4cf581d122fe81fbf44410287592b38949d
coderaiser <coderaiser@cloudcmd.io>
2019-07-11 15:06:37 +0300

    fix(madrun) options

--- test/fix.js
+++ test/fix.js
@@ -5,11 +5,15 @@ const fix = require('../lib/fix');
 const fixture = require('./fixture/fix');
 
 test('madrun: fix', (t) => {
+    const options = {
+        fixCount: 1,
+    };
+    
     const result = fix(`
         module.exports = {
             'hello': 'world'
         };
-    `);
+    `, options);
     
     t.deepEqual(result, fixture, 'should equal');
     t.end();
