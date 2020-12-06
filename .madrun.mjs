a5ab2d7ba53ef2d146f4099f2a6faf2888d162ad
coderaiser <coderaiser@cloudcmd.io>
2020-12-06 21:30:33 +0200

    chore(madrun) lint

--- .madrun.mjs
+++ .madrun.mjs
@@ -1,14 +1,14 @@
 'use strict';
 
-const {run} = require('.');
+import {run} from './lib/madrun.js';
 
-module.exports = {
+export default {
     'lint': () => 'putout .',
     'fix:lint': () => run('lint', '--fix'),
     'test': () => `tape 'test/**/*.js' 'lib/**/*.spec.js'`,
     'watch:test': async () => await run('watcher', await run('test')),
     'watch:tape': () => 'nodemon -w test -w lib --exec tape',
-    'watch:coverage:base': async () => run('watcher', `nyc ${await run('test')}`),
+    'watch:coverage:base': async () => await run('watcher', `nyc ${await run('test')}`),
     'watch:coverage:tape': () => run('watcher', 'nyc tape'),
     'watch:coverage': () => run('watch:coverage:base'),
     'watch:lint': () => run('watcher', run('lint')),
